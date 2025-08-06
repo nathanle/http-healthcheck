3c230cce4ecdb6a158dd3686cdfc89577ba70280
Nathan LeSueur <nlesueur@gmail.com>
2025-08-05 22:44:55 -0400

    Working overall, but want possibly more error details

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,27 @@ use axum::{
     routing::get,
     Router,
 };
-use std::time::Duration;
+//use std::time::Duration;
+use std::collections::HashMap;
+use serde_json::json;
 use reqwest;
 
 
-async fn get_ip() -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
-    let response = match client.get("https://httpbin.org/ip")
-        .timeout(Duration::from_secs(10))  
-        .send()
-        .await {
-            Ok(res) => {
-                if res.status().is_client_error() {
-                    Err(format!("Server returned a client error: {}", res.status()).into())
-                } else if res.status().is_server_error() {
-                    return Err(format!("Server returned a error, check your request data: {}", res.status()).into());
-                }               
-                else {
-                    Ok(res)
-                }
-            }
-            Err(err) => {
-                if err.is_connect() {
-                    return Err("Failed to connect to the server. Please make sure the server is running.".into());
-                }
-                else if err.is_timeout() {
-                    return Err(format!("Request timed out: {}", err).into());
-                } else {
-                    return Err(Box::new(err));
-                }
-            }
-        };
-    println!("{:?}", response);
-    // Handle the response and extract JSON
-    let result_json: serde_json::Value = match response {
-        Ok(res) => match res.json().await {
-            Ok(json) => json,
-            Err(err) => return Err(Box::new(err)),
-        },
-        Err(err) => return Err(err),
-    };
- 
-    Ok(result_json)
+async fn get_ip() -> Result<String, Box<dyn std::error::Error>> {
+    let response = reqwest::get("http://httpbin.org/ge").await?;
+    //let headers = response.headers();
+    let mut headers_json_map = HashMap::new();
+    for (name, value) in response.headers().iter() {
+        headers_json_map.insert(
+            name.to_string(),
+            value.to_str()?.to_string(), // Convert HeaderValue to String
+        );
+    }
+    headers_json_map.insert("Response".to_string(), response.status().to_string());
+
+    let json_output = json!(headers_json_map);
+
+    Ok(json_output.to_string())
 }
 
 fn print_type<T>(_: &T) {
@@ -52,17 +31,13 @@ fn print_type<T>(_: &T) {
 #[tokio::main]
 async fn main() {
     //let data: String;
-    match get_ip().await {
-        Ok(data) => println!("Success: {}", data),
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    let r = match get_ip().await {
+        Ok(data) => data,
+        Err(e) => e.to_string(),
+    };
+    println!("{r}");
 
-    //let mut app;
-    //
-    
-
-    let app = Router::new().route("/healthcheck", get(|| async { "200 OK" }));
-    print_type(&app);
+    let app = Router::new().route("/healthcheck", get(|| async {r}));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
